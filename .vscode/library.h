b9920464399e63fad7857181fe86135c2c8b3bea
Faiq Dzun-Naja Rahman <168202916+dzunnaja@users.noreply.github.com>
2025-06-09 22:06:56 +0700

    pelengkapan deskripsi fungsi no.3

--- .vscode/library.h
+++ .vscode/library.h
@@ -32,3 +32,31 @@ Deskripsi:
 4. Setiap minggu menampilkan pola hari 1-7 yang sama
 5. Total menampilkan 30 hari (4 minggu - 7 hari + 1 minggu - 2 hari)
 */
+
+void shift_perdokter(){
+    /*
+    Input       : kalendar.csv, nama dokter
+    Output      : jadwal si nama dokter
+    Deskripsi   : 
+    1. Membaca file kalendar.csv
+    2. Meminta nama dokter yang ingin ditampilkan jadwalnya
+    3. Mencari semua baris pada file yang sesuai dengan nama dokter tersebut
+    4. Menampilkan semua shift, hari, dan minggu hari tersebut
+    5. Format tampilan:
+    Minggu ke-n, hari:[hari], shift[shift]
+    dst sampai minggu ke 4    
+    */
+}
+
+void pelanggaran(){
+    /*
+    Input       : kalendar.csv, data_dokter.csv
+    Output      : Menampilkan jumlah pelanggaran preferensi shift.
+    Deskripsi   :
+    1. Membaca data_dokter.csv
+    2. Mencari shift yang tidak menjadi preferensi setiap dokter
+    3. Untuk setiap dokter, hitung berapa kali dokter tersebut mendapatkan shift yang tidak menjadi preferensi
+    4. Menampilkan jumlah pelanggaran preferensi shift
+    5. Format tampilan: menyesuaikan hehe
+    */
+}
