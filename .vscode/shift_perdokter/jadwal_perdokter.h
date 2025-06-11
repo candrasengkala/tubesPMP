128c7663044416aa28c2570127800a5b83bee1cb
Faiq Dzun-Naja Rahman <168202916+dzunnaja@users.noreply.github.com>
2025-06-11 16:50:22 +0700

    Create jadwal_perdokter.h
    bismillah fix

---
+++ .vscode/shift_perdokter/jadwal_perdokter.h
@@ -0,0 +1,25 @@
+#ifndef shift_perdokter
+#define shift_perdokter
+
+void jadwal_perdokter();
+    /*
+    Input       : kalendar.csv, nama dokter
+    Output      : jadwal si nama dokter
+    Deskripsi   : 
+    1. Membaca file kalendar.csv
+    2. Meminta nama dokter yang ingin ditampilkan jadwalnya
+    3. Mencari semua baris pada file yang sesuai dengan nama dokter tersebut
+    4. Menampilkan semua shift, hari, dan minggu hari tersebut
+    5. Format tampilan:
+    Minggu ke-n, hari:[hari], shift[shift]
+    dst sampai minggu ke 4    
+    */
+const char* hari_to_hari(int hari);
+    /*
+    ganti hari dari keterangan angka ke senin selasa dst
+    */
+const char* shift_to_shift (int shift);
+    /*
+    ganti shift dari keterangan angka ke keterangan waktu pagi siang malam
+    */
+#endif
