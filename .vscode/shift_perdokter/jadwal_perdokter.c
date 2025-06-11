fd97c47b9456b8d079d73467ad5124c21665a7e0
Faiq Dzun-Naja Rahman <168202916+dzunnaja@users.noreply.github.com>
2025-06-11 16:49:43 +0700

    Create jadwal_perdokter.c
    bismillah fix

---
+++ .vscode/shift_perdokter/jadwal_perdokter.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "jadwal_perdokter.h"
+const char* hari_to_hari(int hari){
+    switch(hari) {
+        case 1: return "Senin";
+        case 2: return "Selasa";
+        case 3: return "Rabu";
+        case 4: return "Kamis";
+        case 5: return "Jumat";
+        case 6: return "Sabtu";
+        case 7: return "Minggu";
+    }
+}
+const char* shift_to_shift (int shift){
+    switch (shift)
+    {
+        case 1: return "Pagi";
+        case 2: return "Siang";
+        case 3: return "Malam";
+    }
+}
+void jadwal_perdokter(){
+    char line[128], line1[128],nama_dokter[20];
+    int found=0, id_dokter=-1;
+    printf("Masukkan nama dokter yang ingin dicari> ");
+    fgets(nama_dokter, sizeof(nama_dokter), stdin);
+    nama_dokter[strcspn(nama_dokter, "\n")] = 0; // hapus newline
+    FILE *data_dokter=fopen("data_dokter.csv", "r");
+    fgets(line1, sizeof(line1), data_dokter);//agar header tidak terbaca
+    while (fgets(line1, sizeof(line1), data_dokter))
+    {
+        line1[strcspn(line1, "\n")]=0;
+        char *id=strtok(line1, ",");
+        char *nama=strtok(NULL, ",");
+        if (strcmp(nama, nama_dokter) == 0){
+            id_dokter=atoi(id);
+            found=1;
+            break;
+        }
+        
+    }
+    fclose(data_dokter);
+    if (!found)
+    {   
+        printf("Nama dokter tidak ditemukan");
+        return 0;
+    }
+    FILE *kalendar=fopen("kalendar.csv", "r");
+    fgets(line, sizeof(line), kalendar);//agar header tidak terbaca
+    while (fgets(line, sizeof(line), kalendar))
+    {
+        line[strcspn(line, "\n")] = 0;
+        char *hari = strtok(line, ",");
+        int hari_angka=atoi(hari);
+        char *shift = strtok(NULL, ",");
+        int shift_int=atoi(shift);
+        char *dokter_list = strtok(NULL, ",");
+        char *dokter_token = strtok(dokter_list, ";");
+        while (dokter_token != NULL) {
+            if (atoi(dokter_token) == id_dokter) {
+                printf("Hari: %s, Shift: %s\n", hari_to_hari(hari_angka), shift_to_shift(shift_int));
+            }
+            dokter_token = strtok(NULL, ";");
+        }
+    }
+    fclose(kalendar);
+}
