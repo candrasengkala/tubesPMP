8f98ac8fc5d4279ea3eb5e48d33a71440b1c620f
Faris Riza M <13223079@std.stei.itb.ac.id>
2025-06-20 16:45:47 +0700

    Update and rename TampilDokter.h to jadwal_dokter.h

--- Jadwal Dokter/jadwal_dokter.h
+++ Jadwal Dokter/jadwal_dokter.h
@@ -1,5 +1,5 @@
-#ifndef TampilDokter_H
-#define TampilDokter_H
+#ifndef jadwal_dokter_h
+#define jadwal_dokter_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,7 @@
 #define MAX_LINE 256
 #define MAX_NAME 100
 #define MAX_DOCTORS 10
-#define MAX_JADWAL_ENTRIES 50 
+#define MAX_JADWAL_ENTRIES 100
 
 // Struktur untuk menyimpan data dokter
 typedef struct {
@@ -22,18 +22,19 @@ typedef struct {
 
 // Struktur untuk menyimpan data jadwal
 typedef struct {
-    int hari; 
-    int shift; 
+    int hari; // Hari ke-1 sampai 30
+    int shift; // 1: Pagi, 2: Siang, 3: Malam
     int dokter_ids[MAX_DOCTORS];
     int jumlah_dokter;
 } JadwalEntry;
 
 // --- FUNGSI HELPER ---
-int baca_data_dokter(const char* filename, Dokter dokters[], int max_dokter);
-int baca_data_kalendar(const char* filename, JadwalEntry jadwal[], int max_entries);
-const char* cari_nama_dokter(int id, const Dokter dokters[], int jumlah_dokter);
+int readfile_data_dokter(const char* filename, Dokter dokters[], int max_dokter);
+int readfile_kalendar(const char* filename, JadwalEntry jadwal[], int max_entries);
+const char* cari_dokter(int id, const Dokter dokters[], int jumlah_dokter);
 const char* nama_hari(int hari);
 const char* nama_shift(int shift);
+void tampil_jadwal_harian(int hari_ke, const JadwalEntry jadwal[], int total_jadwal, const Dokter dokters[], int total_dokter);
 
 // --- FUNGSI UTAMA ---
 void jadwal_harian(const JadwalEntry jadwal[], int total_jadwal, const Dokter dokters[], int total_dokter);
